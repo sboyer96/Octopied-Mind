d29060c63602d822bbabf191945347ca14020a35
Savannah Boyer <gluteus.infinitum@gmail.com>
2025-02-22 20:29:05 -0500

    Initial Commit

---
+++ Octopied Mind/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Octopied Mind
+//
+//  Created by Savannah on 2/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Octopied Mind/Octopied_MindApp.swift
@@ -0,0 +1,17 @@
+//
+//  Octopied_MindApp.swift
+//  Octopied Mind
+//
+//  Created by Savannah on 2/22/25.
+//
+
+import SwiftUI
+
+@main
+struct Octopied_MindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
